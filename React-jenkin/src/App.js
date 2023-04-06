2550cae1725c3271b0d7fa52cb8f23cf7fd72c5f
Kiruthika2505 <110773975+Kiruthika2505@users.noreply.github.com>
2023-04-06 19:48:25 +0530

    Add files via upload

---
+++ React-jenkin/src/App.js
@@ -0,0 +1,13 @@
+import { Fakestore } from "./Components/Fakestore";
+
+function App() {
+  return (
+    <div >
+       <Fakestore/>
+       <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >learn react
+</a>
+    </div>
+  );
+}
+
+export default App;
